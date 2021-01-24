ab12010919b81924b59cbf7127c2c8112febf232
O. O. Nathan <nateoguns.work@gmail.com>
2021-01-24 02:17:41 +0100

    integrated template service

--- src/services/template.js
+++ src/services/template.js
@@ -1,5 +1,10 @@
 /** */
 import { urls } from './_url';
+import { apiGet} from './_http';
 
 /** */
 const base_url = `${urls.users}/templates`;
+
+export const download = async (type, options = {}) => {
+    return await apiGet(`${base_url}/${type}`, { ...options });
+}
